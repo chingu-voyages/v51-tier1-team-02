77c24d9c8eea5cd7a40c47ae355d93f0c72e9868
Natalie Blumen <natalie.blumen@gmail.com>
2024-09-20 14:33:45 -0400

    add group

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,51 @@ button.addEventListener("click", () => {
     }, 4000);
     
 })
+
+// Add Event
+let events = [
+    {
+        name: "Movie",
+        eventID: 1234
+    },
+    {
+        name: "Outback Steakhouse",
+        eventID: 1235
+    },
+    {
+        name: "Ski Tip",
+        eventID: 12534
+    },
+];
+const addEvent = document.getElementById("addEvent");
+const eventContainer = document.getElementById("eventContainer");
+
+addEvent.addEventListener("click", () => {
+    console.log("clicked");
+
+    // Create a new input element
+    const eventInputBox = document.createElement("input");
+    eventInputBox.type = "text";
+    eventInputBox.placeholder = "Enter event name";
+    
+    const eventInputButton = document.createElement("button");
+    eventInputButton.type = "button";
+    eventInputButton.textContent = "Add";
+
+    eventInputButton.addEventListener("click", () =>{
+        const eventName = eventInputBox.value;
+        console.log(eventName);
+        if(eventName) {
+            const newEvent = {name:eventName, eventID: Date.now() };
+            events.push(newEvent); // Add event to the array
+            console.log("Event add:", newEvent.name);
+            console.log("Event ID:",newEvent.eventID);
+            eventInputBox.value = "";
+        }
+    })
+    
+
+    // Append the input box to the container
+    eventContainer.appendChild(eventInputBox);
+    eventContainer.appendChild(eventInputButton);
+});

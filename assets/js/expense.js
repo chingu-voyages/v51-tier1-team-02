4fa46858dd05c24a0e0417d2740bbc2793138870
Robodlan <Robodlan@users.noreply.github.com>
2024-09-26 11:34:18 -0400

    delete expense" :q

--- assets/js/expense.js
+++ assets/js/expense.js
@@ -5,33 +5,67 @@ let events = [
   { name: "Ski Tip", eventID: 12534, expense: 300 },
 ];
 
+let expenses = [];
+const expenseList = document.getElementById("expenseList");
+const totalAmount = document.getElementById("totalAmount");
 
-let group = []; /* array for push expense group  */
+function addExpense() {
+  const name = document.getElementById("expenseName").value;
+  const amount = parseFloat(document.getElementById("expenseAmount").value);
 
-// display array
-
-/**************************************************/
-
-// Expense functions
-let display = [];
-function expenseDiv() {
-  group.forEach((item, index) => {
-    display = `
-    <div class="expense-container">
-        <h3>Event: ${item.name}</h3>
-        <span> $${item.expense}</span>
-        <button onclick=(${deleteBtn()}) id="delete-btn">Delete</button>
-        <button id="edit-btn" >Edit</button>
-        <p>${index}</p>
-    </div>
-    `;
-  });
-
-  document.getElementById("result").innerHTML += display;
-
-  function deleteBtn() {
-    console.log("hi");
+  if (name && !isNaN(amount) && amount > 0) {
+    const expense = {
+      id: Date.now(),
+      name: name,
+      amount: amount,
+    };
+    expenses.push(expense);
+    render();
+    updateTotal();
+    clearInputs();
+    // split();
+  } else {
+    alert("Nope");
   }
 }
 
+function render() {
+  expenseList.innerHTML = "";
+  expenses.forEach((expen, index) => {
+    const li = document.createElement("li");
+    li.classList.add("expense-item"); /* Todo */
+    li.innerHTML = `
+      <span>${expen.name}: $${expen.amount.toFixed(2)}</span>
+      <input type="number value="${expen.amount}" oninput="editExpense(${
+      expen.id
+    }, this.value)" >
+      <button onclick="deleteExpense(${expen.id})">Delete</button>
+    `;
+    expenseList.appendChild(li);
+  });
+}
+
+function updateTotal() {
+  const total = expenses.reduce((acc, expense) => acc + expense.amount, 0);
+  totalAmount.textContent = total;
+  console.log(expenses);
+}
+
+function deleteExpense(id) {
+  expenses = expenses.filter((expense) => expense.id !== id);
+  render();
+  updateTotal();
+}
+
+function editExpense(id, newAmount) {
+  const expense = expenses.find((exp) => exp.id === id);
+  if (expense) {
+    expense.amount = parseFloat(newAmount) || 0;
+  }
+}
+
+function clearInputs() {
+  document.getElementById("expenseName").value = "";
+  document.getElementById("expenseAmount").value = "";
+}
 /**************************************************/

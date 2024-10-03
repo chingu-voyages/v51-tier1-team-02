e0ee248f2243dc90d48f32688f824854fe41fc28
Robodlan <Robodlan@users.noreply.github.com>
2024-10-03 07:14:30 -0400

    expense_bug_fixed

--- assets/js/expense.js
+++ assets/js/expense.js
@@ -1,3 +1,4 @@
+import { members } from "./arrays.js";
 import { getSelectedEvent } from "./events.js";
 console.log(getSelectedEvent);
 //main check
@@ -5,9 +6,9 @@ console.log(getSelectedEvent);
 // Variables
 let expenses = [];
 let editingExpenseId = null;
+let lastSplit = 0;
 let total = 0;
-let id = null;
-const expenseList = document.getElementById("expenseList");
+
 const totalAmount = document.getElementById("totalAmount");
 const splitAmount = document.getElementById("splitAmount");
 const numberOfPeople = document.getElementById("numberOfPeople");
@@ -22,8 +23,6 @@ let UsDollar = new Intl.NumberFormat("en-US", {
 
 // event handlers
 addButton.addEventListener("click", addExpense);
-document.addEventListener("editButton", startEditingExpense);
-document.addEventListener("deleteButton", deleteExpense);
 
 // Create a new obj of expense
 function addExpense() {
@@ -41,6 +40,7 @@ function addExpense() {
         id: Date.now(),
         name: name,
         amount: amount,
+        members: 0,
         date: dDate,
       };
       expenses.push(expense); /* Events data to Expense data */
@@ -52,7 +52,7 @@ function addExpense() {
         expense.name = name;
         expense.amount = amount;
       }
-      editingExpenseId = null;
+      editingExpenseId = null; /* Reset editing state */
       addButton.innerText = "Add Expense";
     }
 
@@ -71,6 +71,7 @@ function addExpense() {
 }
 
 function render() {
+  const expenseList = document.getElementById("expenseList");
   expenseList.innerHTML = "";
   expenses.forEach((expen) => {
     const li = document.createElement("li");
@@ -79,29 +80,40 @@ function render() {
     ${expen.id}
        <p>${expen.date}</p>
       <p>${expen.name}: $${expen.amount.toFixed(2)}</p>
-      <button id="editButton">Edit</button>     
-      <button id="deleteButton" onclick="deleteExpense(${
-        expen.id
-      })">Delete</button>
+      <button class="edit-btn" data-id="${expen.id}" >Edit</button>     
+      <button class="delete-btn" data-id="${expen.id}">Delete</button>
     `;
     expenseList.appendChild(li);
   });
-}
-// onclick="startEditingExpense(${expen.id}
-function updateTotal() {
-  total = expenses.reduce((acc, expense) => acc + expense.amount, 0);
-  totalAmount.innerText = `${UsDollar.format(total)}`;
+  // Bind click events
+  document.querySelectorAll(".edit-btn").forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      startEditingExpense(parseInt(e.target.dataset.id)); //Get expense ID
+    });
+  });
+
+  document.querySelectorAll(".delete-btn").forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      deleteExpense(parseInt(e.target.dataset.id));
+    });
+  });
 }
 
-function split() {
-  const numPeople = parseInt(numberOfPeople.value);
-  total = parseFloat(totalAmount.textContent.replace(/[^0-9.-]+/g, ""));
-  // Remove currency formatting
-  if (numPeople > 0) {
-    const split = total / numPeople;
-    splitAmount.innerText = `${UsDollar.format(split)}`;
-  } else {
-    splitAmount.textContent = "0";
+function startEditingExpense(id) {
+  const expense = expenses.find((expense) => expense.id === id);
+  if (expense) {
+    warning.innerHTML = `<h1>Edit name || amount || members </h1>`;
+    // Populate inputs fields with the current expense details
+    expenseNameInput.value = `${expense.name}`;
+    expenseAmountInput.value = `${expense.amount}`;
+    numberOfPeople.value = `${expense.members}`;
+
+    // Update the editing expense id
+    editingExpenseId = id;
+
+    // Change the add expense button text to "Update Expense"
+    addButton.innerText = "Update Expense";
+    split();
   }
 }
 
@@ -109,20 +121,23 @@ function deleteExpense(id) {
   expenses = expenses.filter((expense) => expense.id !== id);
   render();
   updateTotal();
+  split();
 }
 
-function startEditingExpense(id) {
-  const expense = expenses.find((expense) => expense.id === id);
-  if (expense) {
-    // Populate inputs fields with the current expense details
-    expenseNameInput.value = `Edit name:  ${expense.name}`;
-    expenseAmountInput.value = `Edit amount ${expense.amount}`;
+function updateTotal() {
+  total = expenses.reduce((acc, expense) => acc + expense.amount, 0);
+  totalAmount.innerText = `${UsDollar.format(total)}`;
+}
 
-    // Update the editing expense id
-    editingExpenseId = id;
-
-    // Change the add expense button text to "Update Expense"
-    addButton.innerText = "Update Expense";
+function split() {
+  lastSplit = parseInt(numberOfPeople.value);
+  total = parseFloat(totalAmount.textContent.replace(/[^0-9.-]+/g, ""));
+  // Remove currency formatting
+  if (lastSplit > 0) {
+    const split = total / lastSplit;
+    splitAmount.innerText = `${UsDollar.format(split)}`;
+  } else {
+    splitAmount.textContent = "0";
   }
 }
 
